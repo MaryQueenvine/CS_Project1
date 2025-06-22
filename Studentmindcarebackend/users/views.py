b4d9585953a977795b78b10ee13b445b0c4ef466
MaryQueenvine <maryqueenvine.mwangi@strathmore.edu>
2025-06-22 16:24:56 +0300

    database correction

--- Studentmindcarebackend/users/views.py
+++ Studentmindcarebackend/users/views.py
@@ -1,35 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from .forms import CustomUserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 
-def register_view(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'register.html', {'form': form})
+def home_view(request):
+    return HttpResponse("<h1>Welcome to StudentMindCare Backend</h1>")
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            if user.role == 'student':
-                return redirect('student_dashboard')
-            elif user.role == 'therapist':
-                return redirect('therapist_dashboard')
-            else:
-                return redirect('admin_dashboard')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
 
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+
 
